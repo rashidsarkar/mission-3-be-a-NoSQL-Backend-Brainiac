9531a0b59b5863968f3152da8b08ea1d85ef05fa
rashidsarker <rashidrock558@gmail.com>
2024-11-30 20:11:50 +0600

    module ses

--- module-11-Building-University-Management-System/course-code/Level2-Batch4-PH-University-Server/src/app.ts
+++ module-11-Building-University-Management-System/course-code/Level2-Batch4-PH-University-Server/src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { userRoutes } from './app/modules/user/user.route';
 import { globalErrorHandler } from './app/middlwares/globalErrorhandler';
 import { notFound } from './app/middlwares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -13,8 +12,7 @@ app.use(cors());
 
 //application routes
 
-app.use('/api/v1/student', StudentRoutes);
-app.use('/api/v1/users', userRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
