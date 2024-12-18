5b7a808aa5517e4192da52b55f5c251855c560bc
rashidsarker <rashidrock558@gmail.com>
2024-12-18 19:20:59 +0600

    `Removed commented out database URLs and added "start" text`

--- module-12-Building-University-Management-System-part-2/src/test.js
+++ module-12-Building-University-Management-System-part-2/src/test.js
@@ -1,4 +1,1 @@
-// DATABASE_URL=mongodb+srv://level-2:PvPo3pTB0XcZDgZf@cluster0.ydmxw3q.mongodb.net/university?retryWrites=true&w=majority&appName=Cluster0
-
-// mongodb+srv://level-2:PvPo3pTB0XcZDgZf@cluster0.ydmxw3q.mongodb.net/university?retryWrites=true&w=majority&appName=Cluster0
-// DATABASE_URL=mongodb://127.0.0.1:27017/university
+16. start 
